93e6cdd7c1ced8b10a564cc14901625173c41526
Daniel <62006811+d4nielix@users.noreply.github.com>
2022-02-28 16:07:15 +0100

    Add files via upload

---
+++ PP05/build/CMakeFiles/PP05.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/d4nielix/Pulpit/PP05/PP05.cpp" "CMakeFiles/PP05.dir/PP05.cpp.o" "gcc" "CMakeFiles/PP05.dir/PP05.cpp.o.d"
+  "/home/d4nielix/Pulpit/PP05/xPic.cpp" "CMakeFiles/PP05.dir/xPic.cpp.o" "gcc" "CMakeFiles/PP05.dir/xPic.cpp.o.d"
+  "/home/d4nielix/Pulpit/PP05/xPixelOpsSTD.cpp" "CMakeFiles/PP05.dir/xPixelOpsSTD.cpp.o" "gcc" "CMakeFiles/PP05.dir/xPixelOpsSTD.cpp.o.d"
+  "/home/d4nielix/Pulpit/PP05/xSepia.cpp" "CMakeFiles/PP05.dir/xSepia.cpp.o" "gcc" "CMakeFiles/PP05.dir/xSepia.cpp.o.d"
+  "/home/d4nielix/Pulpit/PP05/xSeq.cpp" "CMakeFiles/PP05.dir/xSeq.cpp.o" "gcc" "CMakeFiles/PP05.dir/xSeq.cpp.o.d"
+  "/home/d4nielix/Pulpit/PP05/xThreadPool.cpp" "CMakeFiles/PP05.dir/xThreadPool.cpp.o" "gcc" "CMakeFiles/PP05.dir/xThreadPool.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PP05/build/CMakeFiles/PP05.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PP05.dir/PP05.cpp.o"
+  "CMakeFiles/PP05.dir/PP05.cpp.o.d"
+  "CMakeFiles/PP05.dir/xPic.cpp.o"
+  "CMakeFiles/PP05.dir/xPic.cpp.o.d"
+  "CMakeFiles/PP05.dir/xPixelOpsSTD.cpp.o"
+  "CMakeFiles/PP05.dir/xPixelOpsSTD.cpp.o.d"
+  "CMakeFiles/PP05.dir/xSepia.cpp.o"
+  "CMakeFiles/PP05.dir/xSepia.cpp.o.d"
+  "CMakeFiles/PP05.dir/xSeq.cpp.o"
+  "CMakeFiles/PP05.dir/xSeq.cpp.o.d"
+  "CMakeFiles/PP05.dir/xThreadPool.cpp.o"
+  "CMakeFiles/PP05.dir/xThreadPool.cpp.o.d"
+  "PP05"
+  "PP05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PP05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

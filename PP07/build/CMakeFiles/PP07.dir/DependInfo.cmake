f4583bda9eaad23d2509478ee99ba3df15b90c91
Daniel <62006811+d4nielix@users.noreply.github.com>
2022-02-28 16:11:24 +0100

    Add files via upload

---
+++ PP07/build/CMakeFiles/PP07.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/danie/Desktop/PP07/PP07.cpp" "CMakeFiles/PP07.dir/PP07.cpp.obj" "gcc" "CMakeFiles/PP07.dir/PP07.cpp.obj.d"
+  "C:/Users/danie/Desktop/PP07/xOpenCL_Enumerator.cpp" "CMakeFiles/PP07.dir/xOpenCL_Enumerator.cpp.obj" "gcc" "CMakeFiles/PP07.dir/xOpenCL_Enumerator.cpp.obj.d"
+  "C:/Users/danie/Desktop/PP07/xPic.cpp" "CMakeFiles/PP07.dir/xPic.cpp.obj" "gcc" "CMakeFiles/PP07.dir/xPic.cpp.obj.d"
+  "C:/Users/danie/Desktop/PP07/xPixelOpsSTD.cpp" "CMakeFiles/PP07.dir/xPixelOpsSTD.cpp.obj" "gcc" "CMakeFiles/PP07.dir/xPixelOpsSTD.cpp.obj.d"
+  "C:/Users/danie/Desktop/PP07/xSepia.cpp" "CMakeFiles/PP07.dir/xSepia.cpp.obj" "gcc" "CMakeFiles/PP07.dir/xSepia.cpp.obj.d"
+  "C:/Users/danie/Desktop/PP07/xSepia_OpenCL.cpp" "CMakeFiles/PP07.dir/xSepia_OpenCL.cpp.obj" "gcc" "CMakeFiles/PP07.dir/xSepia_OpenCL.cpp.obj.d"
+  "C:/Users/danie/Desktop/PP07/xSeq.cpp" "CMakeFiles/PP07.dir/xSeq.cpp.obj" "gcc" "CMakeFiles/PP07.dir/xSeq.cpp.obj.d"
+  "C:/Users/danie/Desktop/PP07/xThreadPool.cpp" "CMakeFiles/PP07.dir/xThreadPool.cpp.obj" "gcc" "CMakeFiles/PP07.dir/xThreadPool.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PP07/build/CMakeFiles/PP07.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PP07.dir/PP07.cpp.obj"
+  "CMakeFiles/PP07.dir/PP07.cpp.obj.d"
+  "CMakeFiles/PP07.dir/xOpenCL_Enumerator.cpp.obj"
+  "CMakeFiles/PP07.dir/xOpenCL_Enumerator.cpp.obj.d"
+  "CMakeFiles/PP07.dir/xPic.cpp.obj"
+  "CMakeFiles/PP07.dir/xPic.cpp.obj.d"
+  "CMakeFiles/PP07.dir/xPixelOpsSTD.cpp.obj"
+  "CMakeFiles/PP07.dir/xPixelOpsSTD.cpp.obj.d"
+  "CMakeFiles/PP07.dir/xSepia.cpp.obj"
+  "CMakeFiles/PP07.dir/xSepia.cpp.obj.d"
+  "CMakeFiles/PP07.dir/xSepia_OpenCL.cpp.obj"
+  "CMakeFiles/PP07.dir/xSepia_OpenCL.cpp.obj.d"
+  "CMakeFiles/PP07.dir/xSeq.cpp.obj"
+  "CMakeFiles/PP07.dir/xSeq.cpp.obj.d"
+  "CMakeFiles/PP07.dir/xThreadPool.cpp.obj"
+  "CMakeFiles/PP07.dir/xThreadPool.cpp.obj.d"
+  "PP07.exe"
+  "PP07.exe.manifest"
+  "PP07.pdb"
+  "libPP07.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PP07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
